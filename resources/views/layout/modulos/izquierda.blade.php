34ea216b96b448f512a9f2ff0ca0184ae0724d9f
dpaul20 <dpaul_20@hotmail.com>
2019-02-20 23:41:46 -0300

    se configura bootstrap, Se trabaja en el header, menu y slide de ofertas

--- resources/views/layout/modulos/izquierda.blade.php
+++ resources/views/layout/modulos/izquierda.blade.php
@@ -1,13 +1,8 @@
-@extends('layout.app')
-@section('title')
-    {{ config('app.name') }}
-@endsection
-@section('content')
-	<h1>Contenido</h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+<h2>Izquierda</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-@endsection
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
